615073d711c464e22d5583c4102099cb1f929e37
Carolina Calderon <caroelizcalderon@gmail.com>
2024-11-24 17:32:02 +0700

    remove blog

--- eleventy.config.js
+++ eleventy.config.js
@@ -47,30 +47,6 @@ export default async function(eleventyConfig) {
 	eleventyConfig.addPlugin(HtmlBasePlugin);
 	eleventyConfig.addPlugin(InputPathToUrlTransformPlugin);
 
-	eleventyConfig.addPlugin(feedPlugin, {
-		type: "atom", // or "rss", "json"
-		outputPath: "/feed/feed.xml",
-		stylesheet: "pretty-atom-feed.xsl",
-		templateData: {
-			eleventyNavigation: {
-				key: "Feed",
-				order: 4
-			}
-		},
-		collection: {
-			name: "posts",
-			limit: 10,
-		},
-		metadata: {
-			language: "en",
-			title: "Carolina Calderon",
-			base: "https://carolinacalderon.com",
-			author: {
-				name: "Carolina Calderon"
-			}
-		}
-	});
-
 	// Image optimization: https://www.11ty.dev/docs/plugins/image/#eleventy-transform
 	eleventyConfig.addPlugin(eleventyImageTransformPlugin, {
 		// File extensions to process in _site folder
